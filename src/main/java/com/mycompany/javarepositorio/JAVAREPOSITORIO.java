f1435eef91eb5319d895ae6d2c8aa78b4196cfee
Alumno <Alumno@PC15692>
2024-01-15 11:01:39 +0100

    Prueba sistemas

---
+++ src/main/java/com/mycompany/javarepositorio/JAVAREPOSITORIO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javarepositorio;
+
+/**
+ *
+ * @author Alumno
+ */
+public class JAVAREPOSITORIO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
